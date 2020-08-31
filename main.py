9ba259edca3626b9c9b6ccf10c3c89d88f735c87
Lydy-a <70544147+Lydy-a@users.noreply.github.com>
2020-08-31 23:29:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from app_class import *
 
 if __name__ == "__main__":
-    bird = Bird()
-    bird.starting_page()
+  bird = Bird()  # Création de l'objet "oiseau"
+  bird.starting_page() # Lancement de la méthode Starting_page pour avoir le menu START
